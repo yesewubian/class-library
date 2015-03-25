4140adf0f3d5d1cfa8af1b9f13a3a49483770ecd
dearmrli <28981944@qq.com>
2015-03-25 11:47:45 +0800

    添加curl普通请求

--- curl_helper.php
+++ curl_helper.php
@@ -42,3 +42,21 @@
         . ($postname ?: basename($filename))
         . ($mimetype ? ";type=$mimetype" : '');
     }
+    
+    /**
+     * curl 请求 
+     */
+    function curlrequest($url,$data,$method='post'){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_USERAGENT,'Opera/9.80 (Windows NT 6.2; Win64; x64) Presto/2.12.388 Version/12.15');
+        curl_setopt($curl, CURLOPT_HTTPHEADER,array('User-Agent: Opera/9.80 (Windows NT 6.2; Win64; x64) Presto/2.12.388 Version/12.15','Referer: http://someaddress.tld','Content-Type: multipart/form-data'));
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // stop verifying certificate
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true); // enable posting
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data); // post images
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); // if any redirection after upload
+        $r = curl_exec($curl);
+        curl_close($curl);
+        return $r;
+	}
